9209e29ed21f886e59d5f0d19dd1d4dc62d0035d
2018-fxy <522406956@qq.com>
2021-04-25 15:19:02 +0800

    第三次提交

--- sparkThread/src/main/java/ThreadTest/Threadtest1.scala
+++ sparkThread/src/main/java/ThreadTest/Threadtest1.scala
@@ -6,12 +6,22 @@ import org.apache.spark.{SparkConf, SparkContext}
 class ThreadExample extends Thread{
   override def run(){
 
-    val sparkConf = new SparkConf().setMaster("local").setAppName("Threadtest1")
+    if (this.getName().equals("Thread_1"))
+      task("{datas_1/*}")
+    else
+      task("{datas_2/*}")
+
+    Thread.sleep(500)
+
+  }
+
+  def task(path: String){
+    val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
 
     //TODO 执行业务操作
     //1.读取文件，获取一行一行的数据
-    val lines: RDD[String] = sc.textFile("{datas/*}")
+    val lines: RDD[String] = sc.textFile(path)
 
     //2.将一行数据进行拆分，形成一个一个的单词（分词）
     val word: RDD[String] = lines.flatMap(_.split(" "))
@@ -26,27 +36,25 @@ class ThreadExample extends Thread{
     }
     //5.将转换结果采集到控制台打印出来
     val array: Array[(String, Int)] = wordTOCount.collect()
-    println(this.getName)
     array.foreach(println)
     //TODO 关闭连接
     sc.stop()
-//    for(i<- 0 to 5){
-//      println(this.getName()+" - "+i)
-//      Thread.sleep(500)
-//    }
   }
 
 }
 
 
 
+
 object Threadtest1 {
   def main(args: Array[String]): Unit = {
     var t1 = new ThreadExample()
+//    t1.task()
     var t2 = new ThreadExample()
     t1.setName("Thread_1")
     t2.setName("Thread_2")
     t1.start()
+    t1.join()
     t2.start()
 
 
